604f8716531bbc3cb2568db163bd3efc590d858f
sark <sarkahn@hotmail.com>
2016-11-13 22:46:31 -0700

    Setup

---
+++ StevenUniverseUnity/Assets/Scripts/Testing/InternalizedAssets/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StevenUniverse.FanGame.Testing
+{
+    public class Test : MonoBehaviour
+    {
+        void Awake()
+        {
+        }
+    }
+}
