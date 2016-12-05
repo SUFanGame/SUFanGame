df127c81224bfcce3adb26ee0ae6072e18e7feda
sark <sarkahn@hotmail.com>
2016-12-05 11:05:03 -0700

    UI and font testing

---
+++ StevenUniverseUnity/Assets/Scripts/Editor/Util/ForcePointFilter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace SUGame.SUGameEditor.Util
+{
+    public class ForcePointFilter
+    {
+        [MenuItem("Assets/Font/Set To Point")]
+        public static void SetPointFilter( MenuCommand command )
+        {
+            //Debug.Log(command.context.GetType().Name);
+            //var font = command.context as Font;
+
+            //Debug.LogFormat("Font is null {0}:", font == null);
+            var font = Selection.activeObject as Font;
+            font.material.mainTexture.filterMode = FilterMode.Point;
+        }
+        
+        [MenuItem("Assets/Font/Set To Point", true)]
+        public static bool ValidateSetPointFilter()
+        {
+            var font = Selection.activeObject as Font;
+            return font != null;
+        }
+
+        //[MenuItem("Assets/Font/Set To Smooth Raster")]
+        //public static void SetToSmoothRaster()
+        //{
+        //    var objects = Selection.objects;
+        //    if (objects == null)
+        //        return;
+        //    foreach( var o in objects )
+        //    {
+        //        var font = o as Font;
+        //        if (font == null)
+        //            continue;
+                
+        //    }
+        //}
+    }
+}
